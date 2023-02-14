825c72bff8c20df98ab6125ec6a04f4f79b1023a
Joe Rupp <joerpp8@gmail.com>
2023-02-13 17:53:32 -0700

    Add nav route functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <section>
-      <h1>NavBar</h1>
+      <ButtonGroup gap="4">
+        <Button variant="outline" colorScheme="gray">
+          <NavLink to="/">Home</NavLink>
+        </Button>
+        <Button variant="outline" colorScheme="gray">
+          <NavLink to="/about">About</NavLink>
+        </Button>
+        <Button variant="outline" colorScheme="gray">
+          <NavLink to="/contact">Contact</NavLink>
+        </Button>
+      </ButtonGroup>
     </section>
   );
 };
